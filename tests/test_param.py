6da7702bab02dd005e20c0152d4986e72efc06b5
msrocka <michael.srocka@gmail.com>
2022-11-07 17:45:07 +0100

    delete things

--- tests/test_param.py
+++ tests/test_param.py
@@ -19,9 +19,10 @@ class ParamTest(unittest.TestCase):
         self.assertEqual(lca.ParameterScope.GLOBAL_SCOPE, x.parameter_scope)
         self.assertEqual(4, x.value)
         self.assertTrue(x.is_input_parameter)
+        client.delete(x)
 
         self.assertEqual('Y', y.name)
         self.assertEqual('X * 10', y.formula)
         self.assertEqual(lca.ParameterScope.GLOBAL_SCOPE, y.parameter_scope)
         self.assertFalse(y.is_input_parameter)
-        # client.delete(param)
+        client.delete(y)
