ce800e4a28251ad783e129bce6568f7108a5fc22
msrocka <michael.srocka@gmail.com>
2023-01-24 18:22:24 +0100

    all tests green now

--- tests/test_get.py
+++ tests/test_get.py
@@ -1,7 +1,7 @@
 import unittest
 
-import olca_schema as lca
-import olca as ipc
+import olca_ipc as ipc
+import olca_schema as schema
 
 
 class GetTest(unittest.TestCase):
@@ -13,20 +13,23 @@ class GetTest(unittest.TestCase):
 
     def test_get_for_name(self):
         client = ipc.Client()
-        mass = client.get(lca.FlowProperty, name='Mass')
-        self.assertEqual('Mass', mass.name)
+        mass = client.get(schema.FlowProperty, name="Mass")
+        self.assertEqual("Mass", mass.name)
 
     def test_get_for_id(self):
         client = ipc.Client()
-        mass = client.get(lca.FlowProperty, name='Mass')
-        group = client.get(lca.UnitGroup, mass.unit_group.id)
-        print(group)
+        mass = client.get(schema.FlowProperty, name="Mass")
+        group = client.get(schema.UnitGroup, mass.unit_group.id)
         self.assertEqual(
-            'kg',
-            next(filter(lambda u: u.is_ref_unit, group.units)).name)
+            "kg", next(filter(lambda u: u.is_ref_unit, group.units)).name
+        )
 
     def test_get_all(self):
         client = ipc.Client()
-        props = client.get_all(lca.FlowProperty)
-        mass = next(filter(lambda p: p.name == 'Mass', props))
+        props = client.get_all(schema.FlowProperty)
+        mass = next(filter(lambda p: p.name == "Mass", props))
         self.assertIsNotNone(mass)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_qref.py
+++ tests/test_qref.py
@@ -1,19 +1,18 @@
 import unittest
 
-import olca as ipc
+import olca_ipc as ipc
 import olca_schema as lca
 
 
 class TestQRef(unittest.TestCase):
-
     def test_qref(self):
         client = ipc.Client()
-        mass = client.get(lca.FlowProperty, name='Mass')
-        steel = lca.new_product('Steel', mass)
-        client.insert(steel)
-        process = lca.new_process('Steel production')
+        mass = client.get(lca.FlowProperty, name="Mass")
+        steel = lca.new_product("Steel", mass)
+        client.put(steel)
+        process = lca.new_process("Steel production")
         lca.new_exchange(process, steel).is_quantitative_reference = True
-        client.insert(process)
+        client.put(process)
 
         process = client.get(lca.Process, process.id)
         qref = next(e for e in process.exchanges if e.is_quantitative_reference)
@@ -22,3 +21,7 @@ class TestQRef(unittest.TestCase):
 
         client.delete(process)
         client.delete(steel)
+
+
+if __name__ == "__main__":
+    unittest.main()
