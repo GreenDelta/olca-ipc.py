f7f44cb066d2c4a5c9b3a2d7f542a9f2dc6b2b25
msrocka <michael.srocka@gmail.com>
2023-01-24 17:23:03 +0100

    add some more tests to the simple result

--- tests/test_simple_calculation.py
+++ tests/test_simple_calculation.py
@@ -46,6 +46,44 @@ class SimpleCalculatioTest(unittest.TestCase):
         self.assertEqual("e", envi_flow.flow.name)
         self.assertEqual("kg", envi_flow.flow.ref_unit)
 
+    def test_tech_flow_values(self):
+        r = self.result
+        seq = [
+            # todo: scaling factors etc.
+            (r.get_total_requirements(), 2)
+        ]
+        for (values, expected) in seq:
+            value: results.TechFlowValue = next(
+                filter(
+                    lambda x: x.tech_flow.provider.id == self.process.id, values
+                )
+            )
+            self.assertEqual(expected, value.amount)
+
+    def test_envi_flow_values(self):
+        tech_flow = self.get_tech_flow()
+        results = [
+            self.result.get_total_flows(),
+            self.result.get_direct_flows_of(tech_flow),
+            self.result.get_total_flows_of(tech_flow),
+        ]
+        for r in results:
+            value: results.EnviFlowValue = next(
+                filter(lambda x: x.envi_flow.flow.id == self.e.id, r)
+            )
+            self.assertEqual(42, value.amount)
+
+    def test_envi_flow_value(self):
+        envi_flow = self.get_envi_flow()
+        tech_flow = self.get_tech_flow()
+        values = [
+            self.result.get_total_flow_value_of(envi_flow),
+            self.result.get_total_flow_of(envi_flow, tech_flow),
+            self.result.get_direct_flow_of(envi_flow, tech_flow),
+        ]
+        for v in values:
+            self.assertEqual(42, v)
+
     def get_tech_flow(self) -> results.TechFlow:
         tech_flows = self.result.get_tech_flows()
         for tech_flow in tech_flows:
