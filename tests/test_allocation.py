d45947ab7a82fb0bb59252cd4217949a824be27d
msrocka <michael.srocka@gmail.com>
2023-01-24 18:13:38 +0100

    update tests

--- tests/test_allocation.py
+++ tests/test_allocation.py
@@ -8,40 +8,34 @@ import olca_schema.results as res
 
 class AllocationTest(unittest.TestCase):
     """
-    Tests the calculation of results for different allocation
-    methods. Note that this test uses functions that are only
-    available in openLCA 2.x and, thus, do not work with
-    openLCA 1.x.
+    Tests the calculation of results for different allocation methods.
     """
 
     def test_example(self):
         client = ipc.Client()
 
-        # get our quantity and unit, assuming that we are
-        # connected to an openLCA database that contains
-        # these reference data and that there are not
-        # multiple flow properties or units with the
-        # name 'Mass' or ' kg' in this database
+        # get our quantity and unit, assuming that we are connected to an
+        # openLCA database that contains these reference data
         mass = client.get(lca.FlowProperty, name="Mass")
         assert mass is not None
         kg = units.unit_ref("kg")
         assert kg is not None
 
-        # create some flows
+        # create the flows p, q, e
         p = lca.new_product("p", mass)
         q = lca.new_product("q", mass)
         e = lca.new_elementary_flow("e", mass)
         for f in [p, q, e]:
-            client.insert(f)
+            client.put(f)
 
-        # create a process with inputs and outputs
+        # create the process with inputs and outputs
         process = lca.new_process("P")
         assert process.id is not None
         lca.new_output(process, p, 1.0, kg).is_quantitative_reference = True
         lca.new_output(process, q, 1.0, kg)
         ex = lca.new_output(process, e, 1.0, kg)
 
-        # define some allocation factors
+        # define the allocation factors
         process.default_allocation_method = lca.AllocationType.CAUSAL_ALLOCATION
         lca.new_physical_allocation_factor(process, p, 0.25)
         lca.new_physical_allocation_factor(process, q, 0.75)
@@ -51,20 +45,19 @@ class AllocationTest(unittest.TestCase):
         lca.new_causal_allocation_factor(process, q, 0.9, ex)
 
         # save the process and create the product system
-        client.insert(process)
+        client.put(process)
         system = client.create_product_system(process.id)
         assert system is not None
         setup = res.CalculationSetup()
-        setup.target = system
         setup.target = lca.Ref(model_type="ProductSystem", id=system.id)
         setup.amount = 500
         setup.unit = units.unit_ref("g")
 
         expected = [
-            (lca.AllocationType.PHYSICAL_ALLOCATION, 0.125),
-            (lca.AllocationType.ECONOMIC_ALLOCATION, 0.2),
-            (lca.AllocationType.CAUSAL_ALLOCATION, 0.05),
-            (lca.AllocationType.USE_DEFAULT_ALLOCATION, 0.05),
+            (lca.AllocationType.PHYSICAL_ALLOCATION, 0.25 / 2),
+            (lca.AllocationType.ECONOMIC_ALLOCATION, 0.4 / 2),
+            (lca.AllocationType.CAUSAL_ALLOCATION, 0.1 / 2),
+            (lca.AllocationType.USE_DEFAULT_ALLOCATION, 0.1 / 2),
             (lca.AllocationType.NO_ALLOCATION, 0.5),
             (None, 0.5),
         ]
@@ -82,7 +75,7 @@ class AllocationTest(unittest.TestCase):
             )
             self.assertFalse(fr.envi_flow.is_input)
             self.assertAlmostEqual(value, fr.amount)
-            client.dispose(result)
+            result.dispose()
             print(" ... success")
 
         # set drop to False if you want to inspect the process in openLCA
@@ -90,3 +83,7 @@ class AllocationTest(unittest.TestCase):
         if drop:
             for each in (system, process, p, q, e):
                 client.delete(each)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_crud.py
+++ tests/test_crud.py
@@ -5,25 +5,24 @@ import olca_schema as lca
 
 
 class CrudTest(unittest.TestCase):
-
     def test_unit_group(self):
         client = ipc.Client()
 
         # create a unit group
-        units = lca.new_unit_group('Units of mass', 'kg')
+        units = lca.new_unit_group("Units of mass", "kg")
         client.put(units)
         units = client.get(lca.UnitGroup, units.id)
-        self.assertEqual('kg', units.units[0].name)
+        self.assertEqual("kg", units.units[0].name)
 
         # create a flow property
-        mass = lca.new_flow_property('Mass', units)
-        client.insert(mass)
+        mass = lca.new_flow_property("Mass", units)
+        client.put(mass)
         mass = client.get(lca.FlowProperty, mass.id)
         self.assertEqual(units.id, mass.unit_group.id)
 
         # update the unit group
         units.default_flow_property = mass.to_ref()
-        client.update(units)
+        client.put(units)
         units = client.get(lca.UnitGroup, units.id)
         self.assertEqual(mass.id, units.default_flow_property.id)
 
@@ -36,15 +35,16 @@ class CrudTest(unittest.TestCase):
         client.close()
 
     def test_process(self):
-        units = lca.new_unit_group('Mass units', 'kg')
-        mass = lca.new_flow_property('Mass', units)
-        steel = lca.new_product('Steel', mass)
-        process = lca.new_process('Steel production')
+        units = lca.new_unit_group("Mass units", "kg")
+        mass = lca.new_flow_property("Mass", units)
+        steel = lca.new_product("Steel", mass)
+        process = lca.new_process("Steel production")
         lca.new_output(process, steel).is_quantitative_reference = True
 
         with ipc.Client() as client:
             for e in [units, mass, steel, process]:
-                client.insert(e)
+                client.put(e)
+            assert process.id is not None
             process = client.get(lca.Process, process.id)
 
             output = process.exchanges[0]
@@ -56,3 +56,7 @@ class CrudTest(unittest.TestCase):
 
             for e in [process, steel, mass, units]:
                 client.delete(e)
+
+
+if __name__ == "__main__":
+    unittest.main()
