8e8ebb403683bc9993fc9c9434d40a599226a05a
msrocka <michael.srocka@gmail.com>
2023-01-24 18:26:08 +0100

    remove the gen-model scripts
    these things are now in the olca-schema repo

--- scripts/makedoc.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem we use pdoc3 to generate our documentation
-rem see https://github.com/pdoc3/pdoc
-
-cd ..
-
-del /s /q docs\*.html
-pdoc --html --force --config latex_math=True --output-dir docs olca
-cd scripts
