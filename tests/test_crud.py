dec920a8ab765705b583fe39216272c759b204ba
msrocka <michael.srocka@gmail.com>
2023-02-07 15:34:29 +0100

    MC simulation green for jsonrpc

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,6 +1,6 @@
 import unittest
 
-import olca_schema as lca
+import olca_schema as o
 
 from config import client
 
@@ -9,40 +9,40 @@ class CrudTest(unittest.TestCase):
     def test_unit_group(self):
 
         # create a unit group
-        units = lca.new_unit_group("Units of mass", "kg")
+        units = o.new_unit_group("Units of mass", "kg")
         client.put(units)
-        units = client.get(lca.UnitGroup, units.id)
+        units = client.get(o.UnitGroup, units.id)
         self.assertEqual("kg", units.units[0].name)
 
         # create a flow property
-        mass = lca.new_flow_property("Mass", units)
+        mass = o.new_flow_property("Mass", units)
         client.put(mass)
-        mass = client.get(lca.FlowProperty, mass.id)
+        mass = client.get(o.FlowProperty, mass.id)
         self.assertEqual(units.id, mass.unit_group.id)
 
         # update the unit group
         units.default_flow_property = mass.to_ref()
         client.put(units)
-        units = client.get(lca.UnitGroup, units.id)
+        units = client.get(o.UnitGroup, units.id)
         self.assertEqual(mass.id, units.default_flow_property.id)
 
         # delete everything
         client.delete(mass)
         client.delete(units)
-        self.assertIsNone(client.get(lca.UnitGroup, units.id))
-        self.assertIsNone(client.get(lca.FlowProperty, mass.id))
+        self.assertIsNone(client.get(o.UnitGroup, units.id))
+        self.assertIsNone(client.get(o.FlowProperty, mass.id))
 
     def test_process(self):
-        units = lca.new_unit_group("Mass units", "kg")
-        mass = lca.new_flow_property("Mass", units)
-        steel = lca.new_product("Steel", mass)
-        process = lca.new_process("Steel production")
-        lca.new_output(process, steel).is_quantitative_reference = True
+        units = o.new_unit_group("Mass units", "kg")
+        mass = o.new_flow_property("Mass", units)
+        steel = o.new_product("Steel", mass)
+        process = o.new_process("Steel production")
+        o.new_output(process, steel).is_quantitative_reference = True
 
         for e in [units, mass, steel, process]:
             client.put(e)
         assert process.id is not None
-        process = client.get(lca.Process, process.id)
+        process = client.get(o.Process, process.id)
 
         output = process.exchanges[0]
         self.assertEqual(steel.id, output.flow.id)
---
+++ tests/test_mc_sim.py
@@ -0,0 +1,70 @@
+import unittest
+import statistics as stats
+
+import olca_schema as o
+import olca_schema.results as r
+from config import client
+
+
+class MCSimTest(unittest.TestCase):
+    def test_sim(self):
+
+        # create units and flows
+        units = o.new_unit_group("Units of mass", "kg")
+        mass = o.new_flow_property("Mass", units)
+        p = o.new_product("p", mass)
+        q = o.new_product("q", mass)
+        e = o.new_elementary_flow("e", mass)
+
+        # create the process
+        process = o.new_process("P")
+        qref = o.new_output(process, p, amount=0.5)
+        qref.is_quantitative_reference = True
+        o.new_output(process, q, amount=0.5)
+        o.new_output(process, e, amount=2).uncertainty = o.Uncertainty(
+            distribution_type=o.UncertaintyType.NORMAL_DISTRIBUTION,
+            mean=2,
+            sd=0.1,
+        )
+        o.new_physical_allocation_factor(process, p, 0.2)
+        o.new_physical_allocation_factor(process, q, 0.8)
+
+        # create the LCIA indicator and method
+        i = o.new_impact_category("i")
+        o.new_impact_factor(i, e, value=2)
+        method = o.new_impact_method("M", i)
+
+        client.put_all(units, mass, p, q, e, process, i, method)
+
+        setup = r.CalculationSetup(
+            allocation=o.AllocationType.PHYSICAL_ALLOCATION,
+            target=o.as_ref(process),
+            amount=1,
+            impact_method=o.as_ref(method),
+        )
+
+        # run simulations and collect indicator results
+        xs = []
+        result = client.simulate(setup)
+
+        def put_next():
+            result.wait_until_ready()
+            xi = result.get_total_impact_value_of(o.as_ref(i))
+            xs.append(xi.amount)
+
+        put_next()
+        for _ in range(0, 9):
+            result.simulate_next()
+            put_next()
+
+        mean = stats.mean(xs)
+        self.assertAlmostEqual(1.6, mean, delta=0.4)
+        # print(xs)
+
+        # clean up
+        result.dispose()
+        client.delete_all(method, i, process, e, p, q, mass, units)
+
+
+if __name__ == "__main__":
+    unittest.main()
