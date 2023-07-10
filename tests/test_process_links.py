d0606fcceb64cd5920ef2ddc448a852a5ee2dccf
msrocka <michael.srocka@gmail.com>
2023-07-10 15:31:18 +0200

    creating process links fails with openLCA 2.0.0

---
+++ tests/test_process_links.py
@@ -0,0 +1,59 @@
+import unittest
+
+import olca_schema as o
+
+from config import client
+
+
+class ProcessLinkTest(unittest.TestCase):
+
+    def setUp(self):
+        units = o.new_unit_group("Units of mass", "kg")
+        mass = o.new_flow_property("Mass", units)
+        e = o.new_elementary_flow("e", mass)
+        p = o.new_product("p", mass)
+        q = o.new_product("q", mass)
+
+        P = o.new_process("P")
+        o.new_output(P, p, amount=1).is_quantitative_reference = True
+        o.new_output(P, e, amount=2)
+
+        Q = o.new_process("Q")
+        qref = o.new_output(Q, q, amount=1)
+        qref.is_quantitative_reference = True
+        linked_exchange = o.new_input(Q, p, 21)
+
+        client.put_all(units, mass, e, p, q, P, Q)
+        self.entities = [units, mass, e, p, q, P, Q]
+
+        system = o.ProductSystem(name="System Q")
+        system.ref_process = Q.to_ref()
+        system.ref_exchange = o.ExchangeRef(internal_id=qref.internal_id)
+        system.target_amount = 1
+        system.target_flow_property = qref.flow_property
+        system.target_unit = qref.unit
+
+        system.processes = [Q.to_ref(), P.to_ref()]
+        system.process_links = [
+            o.ProcessLink(
+                provider=P.to_ref(),
+                process=Q.to_ref(),
+                exchange=o.ExchangeRef(internal_id=linked_exchange.internal_id),
+                flow=p.to_ref(),
+            )
+        ]
+        client.put(system)
+        self.system = system
+
+    def tearDown(self):
+        client.delete_all(*self.entities)
+        client.delete(self.system)
+
+    def test_process_links(self):
+        system = client.get(o.ProductSystem, self.system.id)
+        self.assertIsNotNone(system.process_links)
+        self.assertEquals(1, len(system.process_links))
+
+
+if __name__ == "__main__":
+    unittest.main()
